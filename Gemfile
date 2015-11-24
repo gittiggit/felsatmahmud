cd159974a26ee62e7c87a5b17dfb3745cf5b393e
mahmudrahman <mailtommrr@gmail.com>
2015-11-24 11:04:18 +0000

    Commited in the signup-ligin-logout branch

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ group :development, :test do
   gem 'byebug',      '3.4.0'
   gem 'web-console', '2.0.0.beta3'
   gem 'spring',      '1.1.3'
+  gem 'puma',           '2.11.1'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@ Rails.application.routes.draw do
   
   
 
+  get 'sessions/new'
+
   root 'pages#home'
   get  "pages/home"
   get "help" => "pages#help"
   get  "about" => "pages#about"
 
-get "signup" => 'users#new'
-
-
+  get "signup" => 'users#new'
+  resources :users
+   
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+ 
 end
